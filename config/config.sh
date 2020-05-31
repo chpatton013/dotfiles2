6223cfe74afea07c1211d6892e4ead02c6b61b7f
Christopher Patton <chpatton013@gmail.com>
2020-05-30 21:32:15 -0700

    Remove ubuntu-specifics from config

--- config/config.sh
+++ config/config.sh
@@ -3,11 +3,6 @@ set -euo pipefail
 
 script_dir="$(builtin cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-if [ ! -f /usr/bin/ansible-playbook ]; then
-  sudo apt-get update
-  sudo apt-get install --assume-yes ansible
-fi
-
 ansible-playbook "$script_dir/config.playbook.yml" \
   --extra-vars="dotfiles_src_dir=$script_dir/files" \
   --extra-vars="@$script_dir/config.vars.yml" \
