76e3ede50a11aabd78f100b1dc47b6da89c3be43
Christopher Patton <chpatton013@gmail.com>
2020-06-23 10:05:38 -0700

    Silence compinit in zshrc

--- config/files/zshrc/1-modules.zsh
+++ config/files/zshrc/1-modules.zsh
@@ -1,13 +1,14 @@
 # Enable completion support.
 autoload -U compinit complete complist computil
+compinit -u >/dev/null
 
 # Prompt customization support.
 autoload -U promptinit
+promptinit
 
 # Enable color support.
 autoload -U colors
+colors
 
 # Enable regex support.
 autoload -U regex
-
-colors && compinit -u && promptinit
