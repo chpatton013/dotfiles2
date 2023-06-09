5a55e206a057eb5bca80d63476530e7cb7ef0ac6
Christopher Patton <chpatton013@gmail.com>
2023-06-08 23:20:15 -0700

    Trim usage of apt_key module

--- setup-ubuntu/setup.sh
+++ setup-ubuntu/setup.sh
@@ -8,6 +8,10 @@ if [ ! -f /usr/bin/ansible-playbook ]; then
   sudo apt-get install --assume-yes ansible
 fi
 
+if [ ! -d ~/.ansible/collections/ansible_collections/community/general ]; then
+  ansible-galaxy collection install community.general
+fi
+
 if [ "$(id --user)" = 0 ]; then
   ansible-playbook "$script_dir/setup.playbook.yml" "$@"
 else
