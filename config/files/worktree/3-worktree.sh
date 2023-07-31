819e4d7ad2454b2d51b5245352969985a5ca8c96
Christopher Patton <chpatton013@gmail.com>
2023-07-31 13:13:51 -0700

    move worktree functions into designated task files

--- config/files/worktree/3-worktree.sh
+++ config/files/worktree/3-worktree.sh
@@ -1,3 +1,15 @@
 if [ -d "$(worktree_data_dir)" ]; then
   source "$(worktree_data_dir)/worktree.sh"
+
+  function wt() {
+    worktree "$@"
+  }
+
+  function wt_create() {
+    worktree_create "$@"
+  }
+
+  function wt_resume() {
+    worktree_resume "$@"
+  }
 fi
