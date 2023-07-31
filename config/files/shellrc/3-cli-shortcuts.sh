04814ad8010cb9b2b6ab144faa3fc74912e9a16c
Christopher Patton <chpatton013@gmail.com>
2023-07-31 14:19:26 -0700

    Move vim shell functions to associated task

--- config/files/shellrc/3-cli-shortcuts.sh
+++ config/files/shellrc/3-cli-shortcuts.sh
@@ -10,29 +10,6 @@ function la() {
   l --almost-all "$@"
 }
 
-function v() {
-  "$EDITOR" "$@"
-}
-
-function vo() {
-  v -o "$@"
-}
-
-function vO() {
-  v -O "$@"
-}
-
-function vp() {
-  v -p "$@"
-}
-
-function vprof() {
-  local output_file="$(mktemp)"
-  v "+profile start $output_file" "+profile func *" "+profile file *" "$@"
-  echo reading profile from $output_file
-  less "$output_file"
-}
-
 function catr() {
   find "$@" -type f | xargs --no-run-if-empty cat
 }
---
+++ config/files/vim/3-vim.sh
@@ -0,0 +1,22 @@
+function v() {
+  "$EDITOR" "$@"
+}
+
+function vo() {
+  v -o "$@"
+}
+
+function vO() {
+  v -O "$@"
+}
+
+function vp() {
+  v -p "$@"
+}
+
+function vprof() {
+  local output_file="$(mktemp)"
+  v "+profile start $output_file" "+profile func *" "+profile file *" "$@"
+  echo reading profile from $output_file
+  less "$output_file"
+}
