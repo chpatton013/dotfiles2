791ac0d0c1ad7f36ccfed9cce910ef10c8419a5a
Christopher Patton <chpatton013@gmail.com>
2023-07-31 12:58:34 -0700

    fzf options; strip cwd and follow symlinks

--- config/files/fzf/3-fzf.sh
+++ config/files/fzf/3-fzf.sh
@@ -1,1 +1,2 @@
-export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
+export FZF_DEFAULT_COMMAND="fd --type f --strip-cwd-prefix --hidden --follow --exclude .git"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
