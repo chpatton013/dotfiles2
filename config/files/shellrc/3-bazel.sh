2e830e3ca56a353f7604a205e3d662ed93d4e0fc
Christopher Patton <chpatton013@gmail.com>
2023-08-01 13:06:58 -0700

    Fix all issues with config sync

--- config/files/shellrc/3-bazel.sh
+++ config/files/shellrc/3-bazel.sh
@@ -7,16 +7,7 @@ function skyquery() {
 }
 
 function filequery() {
-  xargs -I{} \
-    bazel query "$@" --universe_scope=//... --order_output=no \
-    'kind(rule, allrdeps(set({}), 1))'
-}
-
-function dir-ccb-somepath() {
-  dirpath="$1"
-  shift
-  universe="//$dirpath"
-  files="$(find "$dirpath" -type f | tr '[[:space:]]' ' ')"
-  targets="$(bazel query --keep_going --universe_scope="$universe" --order_output=no "kind(rule, allrdeps(set($files), 1))" 2>/dev/null | tr '[[:space:]]' ' ')"
-  bazel query "$@" "somepath(//vehicle:l3_ccb_regulated_package, set($targets))"
+  files=$(cat | tr '[[:space:]]' ' ')
+  bazel query "$@" --universe_scope=//... --order_output=no \
+    "kind(rule, allrdeps(set($files), 1))"
 }
