16cf4b967f18049272a74089f7c0606af5f85e90
Christopher Patton <chpatton013@gmail.com>
2023-11-05 20:42:39 -0800

    rebase into path fixes

--- config/files/shellrc/2-pathlist-macos.sh
+++ config/files/shellrc/2-pathlist-macos.sh
@@ -14,14 +14,35 @@ function find_gnu_packages() {
   done
 }
 
-for dir in $(find_gnu_packages -name gnubin); do
-  if [ -z "$PATH" ]; then
-    PATH="$dir"
+function prepend_pathlist() {
+  local pathlist; pathlist="$1"
+  local entry; entry="$2"
+  if [ -z "$pathlist" ]; then
+    echo "$entry"
   else
-    PATH="$dir:$PATH"
+    echo "$entry:$pathlist"
   fi
+}
+
+function append_pathlist() {
+  local pathlist; pathlist="$1"
+  local entry; entry="$2"
+  if [ -z "$pathlist" ]; then
+    echo "$entry"
+  else
+    echo "$pathlist:$entry"
+  fi
+}
+
+PATH="$(prepend_pathlist "$PATH" /opt/homebrew/sbin:/opt/homebrew/bin)"
+# Prepend GNU bins so they take precedence over BSD bins
+for dir in $(find_gnu_packages -name gnubin); do
+  PATH="$(prepend_pathlist "$PATH" "$dir")"
 done
 export PATH
+
+LD_LIBRARY_PATH="$(append_pathlist "$LD_LIBRARY_PATH" /opt/homebrew/lib)
+export LD_LIBRARY_PATH
 
 for dir in $(find_gnu_packages -name gnuman); do
   if [ -z "$MANPATH" ]; then
@@ -30,4 +51,5 @@ for dir in $(find_gnu_packages -name gnuman); do
     MANPATH="$dir:$MANPATH"
   fi
 done
+MANPATH="$(append_pathlist "$MANPATH" /opt/homebrew/share/man)
 export MANPATH
