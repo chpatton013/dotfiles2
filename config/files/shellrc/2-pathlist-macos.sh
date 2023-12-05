144412782d3dfa5f42e4123d1407f19d6e6de6c6
Christopher Patton <chpatton013@gmail.com>
2023-12-04 21:22:50 -0800

    Fix macos pathlist

--- config/files/shellrc/2-pathlist-macos.sh
+++ config/files/shellrc/2-pathlist-macos.sh
@@ -1,17 +1,18 @@
 function find_gnu_packages() {
-  local gnu_packages=(
-    coreutils
-    findutils
-    gawk
-    gnu-indent
-    gnu-sed
-    gnu-tar
-    gnu-which
-    grep
-  )
-  for dir in ${gnu_packages[@]}; do
-    find "/opt/homebrew/Cellar/$dir" "$@"
-  done
+  # Performance optimization; instead of searching through the whole homebrew
+  # opt dir, pre-calculate and track this list.
+  local name; name="$1"
+  cat <<EOF
+/opt/homebrew/opt/coreutils/libexec/$name
+/opt/homebrew/opt/findutils/libexec/$name
+/opt/homebrew/opt/gawk/libexec/$name
+/opt/homebrew/opt/gnu-indent/libexec/$name
+/opt/homebrew/opt/gnu-sed/libexec/$name
+/opt/homebrew/opt/gnu-tar/libexec/$name
+/opt/homebrew/opt/gnu-which/libexec/$name
+/opt/homebrew/opt/grep/libexec/$name
+/opt/homebrew/opt/libtool/libexec/$name
+EOF
 }
 
 function prepend_pathlist() {
@@ -36,20 +37,17 @@ function append_pathlist() {
 
 PATH="$(prepend_pathlist "$PATH" /opt/homebrew/sbin:/opt/homebrew/bin)"
 # Prepend GNU bins so they take precedence over BSD bins
-for dir in $(find_gnu_packages -name gnubin); do
+for dir in $(find_gnu_packages gnubin); do
   PATH="$(prepend_pathlist "$PATH" "$dir")"
 done
 export PATH
 
-LD_LIBRARY_PATH="$(append_pathlist "$LD_LIBRARY_PATH" /opt/homebrew/lib)
-export LD_LIBRARY_PATH
-
-for dir in $(find_gnu_packages -name gnuman); do
-  if [ -z "$MANPATH" ]; then
-    MANPATH="$dir"
-  else
-    MANPATH="$dir:$MANPATH"
-  fi
+# Prepend GNU mans so they take precedence over BSD bins
+for dir in $(find_gnu_packages gnuman); do
+  MANPATH="$(prepend_pathlist "$MANPATH" "$dir")"
 done
-MANPATH="$(append_pathlist "$MANPATH" /opt/homebrew/share/man)
+MANPATH="$(append_pathlist "$MANPATH" /opt/homebrew/share/man)"
 export MANPATH
+
+LD_LIBRARY_PATH="$(append_pathlist "$LD_LIBRARY_PATH" /opt/homebrew/lib)"
+export LD_LIBRARY_PATH
