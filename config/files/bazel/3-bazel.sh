d585a4d4471b2ec3f92485eddf5b3a77cabb699c
Christopher Patton <chpatton013@gmail.com>
2024-09-20 13:15:08 -0700

    Add chunk bin tool

--- config/files/bazel/3-bazel.sh
+++ config/files/bazel/3-bazel.sh
@@ -7,7 +7,6 @@ function skyquery() {
 }
 
 function filequery() {
-  files=$(cat | tr '[[:space:]]' ' ')
-  bazel query "$@" --universe_scope=//... --order_output=no \
-    "kind(rule, allrdeps(set($files), 1))"
+  xargs -I{} bazel query "$@" --universe_scope=//... --order_output=no \
+    'kind(rule, allrdeps(set({}), 1))'
 }
