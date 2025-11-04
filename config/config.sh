e5a0b382cc98ac705324ac62737e27ccb86138ab
Christopher Patton <chpatton013@gmail.com>
2025-11-04 10:44:16 -0800

    fix python config

--- config/config.sh
+++ config/config.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 script_dir="$(builtin cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+ansible-galaxy collection install community.general --upgrade
+
 ansible-playbook "$script_dir/config.playbook.yml" \
   --extra-vars="dotfiles_src_dir=$script_dir/files" \
   --extra-vars="@$script_dir/config.vars.yml" \
