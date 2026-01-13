505f4c31bf9f6b307a71f51f44ddfd20cd34404e
Christopher Patton <chpatton013@gmail.com>
2026-01-13 12:12:55 -0800

    Fix some modernization issues

--- setup-ubuntu/setup.sh
+++ setup-ubuntu/setup.sh
@@ -3,14 +3,7 @@ set -euo pipefail
 
 script_dir="$(builtin cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-if [ ! -f /usr/bin/ansible-playbook ]; then
-  sudo apt-get update
-  sudo apt-get install --assume-yes ansible
-fi
-
-if [ ! -d ~/.ansible/collections/ansible_collections/community/general ]; then
-  ansible-galaxy collection install community.general
-fi
+ansible-galaxy collection install community.general --upgrade
 
 if [ "$(id --user)" = 0 ]; then
   ansible-playbook "$script_dir/setup.playbook.yml" "$@"
