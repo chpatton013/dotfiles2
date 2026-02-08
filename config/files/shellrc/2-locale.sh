ec421b4154f6300329f7bf95000d7e2b6e3f4c3f
Christopher Patton <chpatton013@gmail.com>
2026-02-07 17:51:32 -0800

    shell: fix tz var usage

--- config/files/shellrc/2-locale.sh
+++ config/files/shellrc/2-locale.sh
@@ -2,4 +2,15 @@ export LANGUAGE=en_US
 export CHARSET=UTF-8
 export LANG="$LANGUAGE.$CHARSET"
 export LC_COLLATE=C # Sort uppercase before lowercase
-export TZ=America/Los_Angeles
+
+if [ ! -f /etc/localtime ]; then
+  # Only set TZ if this machine does not have a valid timezone file.
+  export TZ=America/Los_Angeles
+  if [ -d /usr/share/zoneinfo ]; then
+    # Explicitly set TZDIR if it exists. When running a process with a non-
+    # default sysroot, if TZDIR is not set, libc will not be able to locate the
+    # zoneinfo file specified by the TZ environment variable unless TZDIR is
+    # explicitly set.
+    export TZDIR=/usr/share/zoneinfo
+  fi
+fi
